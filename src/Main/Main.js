25a60d0fdc36f49dce068aaad6f61ec806c55a72
Vanessa Seva <vanessa.seva@fabrick.com>
2021-06-14 15:00:39 +0200

    first design

---
+++ src/Main/Main.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Main = () => {
+
+
+
+
+    return(
+        <div>
+            <div>
+            <h1 className="title">Weather App</h1>
+            <hr className="separation"/>
+            </div>
+            <div className="container">
+            <div className="weatherBox">
+                <h2 className="title">Today</h2>
+                <p>Ville</p>
+                <p>25°</p>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default Main
