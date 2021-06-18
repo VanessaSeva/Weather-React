63068ee2ecdc435b9f1d30f723b2e4011693a359
Vanessa Seva <vanessa.seva@fabrick.com>
2021-06-18 18:33:25 +0200

    display data

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -3,18 +3,42 @@ import axios from 'axios'
 
 const Main = () => {
 
-    const [data, setData] = useState([]);
+    const api = {
+        key: "76a27df4ea541168980658d2bbc73e19",
+        base: "http://api.openweathermap.org/data/2.5/"
+    }
 
-    useEffect(() => {
-        axios.get('http://api.openweathermap.org/data/2.5/weather?q=Paris&lang=fr&units=metric&appid=76a27df4ea541168980658d2bbc73e19')
-        .then((res) => {
-            setData(res.data)
-            console.log(res.data.main.temp);
-        })
-    }, [])
-       
+    const [data, setData] = useState('');
+    const [weather, setWeather] = useState({})
+
     
+    const search = evt => {
+        if (evt.key === "Enter") {
+            fetch(`${api.base}weather?q=${data}&lang=fr&units=metric&APPID=${api.key}`)
+            .then(res => res.json())
+            .then(result => {
+                setData('');
+                setWeather(result)
+                console.log(result);
+             })
+            
+        }
+    }
+    
+    const dateBuilder = (d) => {
+        let months = ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre",
+        "Novembre", "Décembre"];
+        let days = ["Dimanche","Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
 
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} ${month} ${year}`
+    }
+    
+  
 
     return (
         <div>
@@ -22,13 +46,32 @@ const Main = () => {
             <h1 className="title">Weather App</h1>
             <hr className="separation"/>
             </div>
+            <div>
+                <input type="text" 
+                placeholder="chercher une ville" 
+                onChange={e => setData(e.target.value)} 
+                value={data}
+                onKeyPress={search}
+                />
+            </div>
+          
             <div className="container">
             <div className="weatherBox">
-                <h2 className="title">Today</h2>
-                <p>Ville</p>
-                <p>25</p>
+                <h2 className="title">{dateBuilder(new Date())}</h2>
+                <p>
+            {weather.name} 
+                </p>
+                <p>
+                {weather.main ? Math.round(weather.main.temp) : "°C"} °C                
+                </p>
+                <p>
+            {weather.weather?.[0].main}
+                </p>
+               
             </div>
+          
             </div>
+           
         </div>
     )
 }
